66e04e68a7576e33293dd44ca753f17dbf1212cc
Philipp Kuznetsov <philipp@bestdoctor.ru>
2019-12-07 17:34:59 +0300

    Move login to LivetexExtractor class

--- src/extractor/extractor.py
+++ src/extractor/extractor.py
@@ -1,13 +1,12 @@
-import asyncio
-from aiohttp import ClientSession, TCPConnector, ClientResponse
+from aiohttp import ClientSession, ClientResponse
 import datetime
 import enum
-import csv
-from typing import Tuple, Optional
-from asyncclick import command, option, File, DateTime, STRING
+from typing import Optional
 from backoff import expo, on_predicate
-from config import LIVETEX_BASE_URL, LOGIN_URL, DIALOGS_URL, DIALOG_INFO, MAX_LIMIT, BACKOFF_MAX_TIME
-from helpers import flat
+from config import (
+    LIVETEX_BASE_URL, LOGIN_URL, DIALOGS_URL,
+    DIALOG_INFO, MAX_LIMIT, BACKOFF_MAX_TIME,
+)
 
 
 class EventType(enum.Enum):
@@ -19,22 +18,12 @@ class EventCreator(enum.Enum):
     CLIENT = 'Discourser'
 
 
-async def login(username: str, password: str, session: ClientSession) -> Tuple[str, str]:
-    payload = {'username': username, 'password': password}
-    headers = {'Cookie': 'ng_environment=production-3'}
-    async with session.post(LOGIN_URL, headers=headers, data=payload) as resp:
-        status = (await resp.json())['status']
-        if status != 'ok':
-            raise ValueError('Wrong credentials')
-        return resp.cookies['id'].value, resp.cookies['access_token'].value
-
-
 class LivetexExtractor:
-    def __init__(self, account_id: int, token: str,
+    def __init__(self, username: str, password: str,
                  start: datetime.datetime, end: datetime.datetime,
                  session: ClientSession) -> None:
-        self._id = account_id
-        self._token = token
+        self.username = username
+        self.password = password
         self._session = session
         self._start = start
         self._end = end
@@ -71,6 +60,16 @@ class LivetexExtractor:
                 })
         return messages
 
+    async def login(self, session: ClientSession):
+        payload = {'username': self.username, 'password': self.password}
+        headers = {'Cookie': 'ng_environment=production-3'}
+        async with session.post(LOGIN_URL, headers=headers, data=payload) as resp:
+            status = (await resp.json())['status']
+            if status != 'ok':
+                raise ValueError('Wrong credentials')
+            self._id = resp.cookies['id'].value
+            self._token = resp.cookies['access_token'].value
+
     def _get_message_author(self, topic, event) -> Optional[str]:
         creator = event['creator']
         if creator['creatorType'] == EventCreator.EMPLOYEE.value:
@@ -88,29 +87,3 @@ class LivetexExtractor:
         default_params = {'accountId': self._id}
         default_params.update(params)
         return await self._session.get(method, params=default_params, headers=headers)
-
-
-@command()
-@option('--username', prompt='Enter your username', type=STRING, help='Livetex username')
-@option('--password', prompt='Enter your password', type=STRING, help='Livetex password')
-@option('--start', prompt='Start', type=DateTime(), help='Start extracting from this date')
-@option('--end', prompt='End', type=DateTime(), help='Extract messages till this date')
-@option('--output', prompt='Output', type=File('w'), help='Output file')
-async def main(username, password, start, end, output):
-    tasks = []
-    async with ClientSession(connector=TCPConnector(ssl=False)) as session:
-        account_id, token = await login(username, password, session)
-        extractor = LivetexExtractor(account_id, token, start, end, session)
-        topics_short = await extractor.get_dialogs_short()
-        for topic in topics_short:
-            task = asyncio.ensure_future(extractor.get_dialog_info(topic['topicId']))
-            tasks.append(task)
-
-        data = flat(await asyncio.gather(*tasks))
-
-    dict_writer = csv.DictWriter(output, data[0].keys())
-    dict_writer.writeheader()
-    dict_writer.writerows(data)
-
-if __name__ == '__main__':
-    main(_anyio_backend='asyncio')
---
+++ src/extractor/main.py
@@ -0,0 +1,33 @@
+
+import asyncio
+from aiohttp import ClientSession, TCPConnector
+import csv
+from asyncclick import command, option, File, DateTime, STRING
+from helpers import flat
+from extractor import LivetexExtractor
+
+
+@command()
+@option('--username', prompt='Enter your username', type=STRING, help='Livetex username')
+@option('--password', prompt='Enter your password', type=STRING, help='Livetex password')
+@option('--start', prompt='Start', type=DateTime(), help='Start extracting from this date')
+@option('--end', prompt='End', type=DateTime(), help='Extract messages till this date')
+@option('--output', prompt='Output', type=File('w'), help='Output file')
+async def main(username, password, start, end, output):
+    tasks = []
+    async with ClientSession(connector=TCPConnector(ssl=False)) as session:
+        extractor = LivetexExtractor(username, password, start, end, session)
+        await extractor.login(session)
+        topics_short = await extractor.get_dialogs_short()
+        for topic in topics_short:
+            task = asyncio.ensure_future(extractor.get_dialog_info(topic['topicId']))
+            tasks.append(task)
+
+        data = flat(await asyncio.gather(*tasks))
+
+    dict_writer = csv.DictWriter(output, data[0].keys())
+    dict_writer.writeheader()
+    dict_writer.writerows(data)
+
+if __name__ == '__main__':
+    main(_anyio_backend='asyncio')
